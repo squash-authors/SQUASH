7a7e56dabe96215ef5fa31a1fbaffb9512a3a706
squash-authors <squashauthors@gmail.com>
2024-10-23 17:08:22 +0100

    Update setup.py

--- SQLAYER/setup.py
+++ SQLAYER/setup.py
@@ -20,7 +20,7 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent",
     ],
-    install_requires=["numpy>=2.0.0", "boto3>=1.34.82"],
+    install_requires=["numpy>=2.0.0", "boto3>=1.34.82", "bitarray==2.5.1"],
     extras_require={},
     python_requires=">=3.11",
 )
